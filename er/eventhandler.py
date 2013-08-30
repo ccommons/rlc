5c12edbda0c6b2ff76e375ac515239338f675033
Therese Bao <itherese@gmail.com>
2013-08-30 16:30:37 -0700

    added register_event() and match_request()

--- er/eventhandler.py
+++ er/eventhandler.py
@@ -1,6 +1,5 @@
 from er.models import Comment as mComment
 from er.models import EvidenceReview as mEvidenceReview
-from er.annotation import comment
 from django.core.urlresolvers import reverse
 
 import logging
@@ -72,6 +71,32 @@ class eventHandler(object):
     def get_preview(self, event):
         return ''
 
+    @classmethod
+    def register_event(cls, *args, **kwargs):
+        """notification API
+           wrapper for create_event(), silent all exceptions
+           returns an event object
+        """
+        try:
+            return cls.create_event(*args, **kwargs)
+        except Exception, ex:
+            logger.error(ex)
+            return None
+
+    @classmethod
+    def create_event(cls, *args, **kwargs):
+        """ subclasses must implement """
+        return None
+
+    @classmethod
+    def match_request(cls, event_model, request):
+        """
+        subclasses should implement
+        takes an Event model object and a HttpRequest object
+        returns True if they match, False otherwise
+        """
+        return False
+
 class commentEventHandler(eventHandler):
     def __config__(self):
         self._resource_model = mComment
@@ -84,12 +109,30 @@ class commentEventHandler(eventHandler):
         return map(lambda x: int(x), pks)
 
     def create_comment_obj(self, event):
+        from er.annotation import comment
         if event and event.resource:
             c = comment()
             c.model_object = event.resource
             c.init_from_model()
             return c
         return None
+
+    @classmethod
+    def create_event(cls, *args, **kwargs):
+        """
+        must take one positional argument (the comment model object),
+        optional keyword argument 'action' and 'remarks'
+        """
+        from er.event import event
+        resource = args[0]
+        event_handler=cls()
+        event_kwargs = dict(event_handler=event_handler, resource=resource)
+        if 'action' in kwargs:
+            event_kwargs['action'] = kwargs['action']
+        if 'remarks' in kwargs:
+            event_kwargs['remarks'] = kwargs['remarks']
+        e = event(**event_kwargs)
+        return e
 
 class commentAnnotationEventHandler(commentEventHandler):
     def __config__(self):
@@ -98,32 +141,48 @@ class commentAnnotationEventHandler(commentEventHandler):
 
     def construct_url(self, event):
         c = self.create_comment_obj(event)
-        if not c:
-            return ''
-        try:
-            # an annotation
-            a = c.root.model_object.annotation
-            return reverse('annotation_one_of_all', kwargs={'doc_id':a.er_doc.id,'annotation_id':a.id,})
-        except Exception, e:
-            logger.error(e)
-            pass
-        return ''
+        a = c.root.model_object.annotation
+        return reverse('annotation_one_of_all', kwargs={'doc_id':a.er_doc.id,'annotation_id':a.id,})
 
     def get_preview(self, event):
         c = self.create_comment_obj(event)
-        if not c:
-            return ''
         comment_text = c.text[:100]
         if c.is_root():
-            try:
-                a = c.annotation
-                if a.doc_block:
-                    return a.doc_block.preview_text
-                else:
-                    return comment_text
-            except:
-                pass
+            a = c.model_object.annotation
+            if a.doc_block:
+                return a.doc_block.preview_text
         return comment_text
+
+    @classmethod
+    def match_request(cls, event_model, request):
+        """
+        subclasses should implement
+        takes an Event model object and a HttpRequest object
+        returns True if they match, False otherwise
+        """
+        doc_id = int(request.resolver_match.kwargs.get('doc_id', ''))
+        annotation_id = int(request.resolver_match.kwargs.get('annotation_id', ''))
+        url_name = request.resolver_match.url_name
+        from er.event import event
+        handler = cls()
+        e = event(model_object=event_model, event_handler=handler)
+        try:
+            c = handler.create_comment_obj(e)
+            a = c.root.model_object.annotation
+            if doc_id == a.er_doc.id and annotation_id == a.id and url_name == 'annotation_one_of_all':
+                return True
+        except:
+            pass
+        return False
+
+class proprevApprovedEventHandler(commentAnnotationEventHandler):
+    def __config__(self):
+        commentAnnotationEventHandler.__config__(self)
+        self._etype = 'proprev_approved'
+
+    def get_preview(self, event):
+        c = self.create_comment_obj(event)
+        return c.root.model_object.annotation.doc_block.preview_text
 
 class commentNewsEventHandler(commentEventHandler):
     def __config__(self):
@@ -132,29 +191,17 @@ class commentNewsEventHandler(commentEventHandler):
 
     def construct_url(self, event):
         c = self.create_comment_obj(event)
-        if not c:
-            return ''
-        try:
-            a = c.root.model_object.news
-            # XXX TODO
-            return ''
-        except Exception, e:
-            logger.error(e)
-            pass
+        a = c.root.model_object.news
+        # XXX TODO
         return ''
 
     def get_preview(self, event):
         c = self.create_comment_obj(event)
-        if not c:
-            return ''
         comment_text = c.text[:100]
         if c.is_root():
-            try:
-                a = c.news
-                # XXX TODO
-                return comment_text
-            except:
-                pass
+            a = c.news
+            # XXX TODO
+            return comment_text
         return comment_text
 
 class EvidenceReviewEventHandler(eventHandler):
@@ -167,21 +214,49 @@ class EvidenceReviewEventHandler(eventHandler):
         return map(lambda x: int(x), pks)
 
     def construct_url(self, event):
-        if not event or not event.resource:
-            return ''
         return reverse('doc_full_view', doc_id=event.resource.id)
 
     def get_preview(self, event):
-        if not event or not event.resource:
-            return ''
         return event.resource.title
+
+    @classmethod
+    def create_event(cls, *args, **kwargs):
+        """
+        must take one positional argument (the EvidenceReview model object),
+        optional keyword argument 'action' and 'remarks'
+        """
+        from er.event import event
+        resource = args[0]
+        event_handler=cls()
+        event_kwargs = dict(event_handler=event_handler, resource=resource)
+        if 'action' in kwargs:
+            event_kwargs['action'] = kwargs['action']
+        if 'remarks' in kwargs:
+            event_kwargs['remarks'] = kwargs['remarks']
+        e = event(**event_kwargs)
+        return e
+
+    @classmethod
+    def match_request(cls, event_model, request):
+        """
+        subclasses should implement
+        takes an Event model object and a HttpRequest object
+        returns True if they match, False otherwise
+        """
+        doc_id = request.resolver_match.kwargs.get('doc_id', '')
+        url_name = request.resolver_match.url_name
+        try:
+            if doc_id == event_model.resource_id and url_name == 'doc_full_view':
+                return True
+        except:
+            pass
+        return False
 
 # XXX TODO: make it a subclass of dict, verify key with etype of handler whenever a key/value pair is set
 EVENT_TYPE_HANDLER_MAP = {
     'comment_annotation' : commentAnnotationEventHandler,
-    'comment_news' : commentNewsEventHandler,
+    #'comment_news' : commentNewsEventHandler,
+    'proprev_approved' : proprevApprovedEventHandler,
     'er' : EvidenceReviewEventHandler,
     #'user' : UserEventHandler,
 }
-
-
