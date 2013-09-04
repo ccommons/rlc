6040cf141a95b49ed0a9a1b84c7260e2684f7c92
Therese Bao <itherese@gmail.com>
2013-09-03 18:16:38 -0700

    added override_ckeditor to context

--- er/views/notification.py
+++ er/views/notification.py
@@ -51,6 +51,7 @@ def get_notifications(request):
             unread_items.append(item)
 
     context = Context({
+        "override_ckeditor" : True,
         "unread" : unread_items,
         "read" : read_items,
     })
