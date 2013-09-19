2f8eeead6cc3643e03c3a21b17ccf462883f4b43
Brian Ward <bri@aem7.net>
2013-09-19 16:59:03 -0700

    reference tooltips and initialization

--- er/static/doc.js
+++ er/static/doc.js
@@ -23,3 +23,31 @@ function verify_json_response(data) {
         }
     }
 }
+
+function references_init() {
+    $('.rlc-reference').click(function(event) {
+        $ref_element = $(event.currentTarget);
+        /* initialize the tooltip if necessary */
+        if ($ref_element.attr("data-toggle") === undefined) {
+            $ref_element.attr("data-toggle", "tooltip");
+            $ref_element.attr("data-trigger", "click");
+            $ref_element.attr("data-placement", "right");
+            var content = "<div class=\"ref-info\">" + $ref_element.attr('data-ref-info') + "</div>";
+            if ($ref_element.attr('url') !== undefined) {
+                content = content + "<a class=\"ref-url\" target=\"_blank\" href=\"" + $ref_element.attr('url') + "\">";
+                content = content + "View at PubMed";
+                content = content + "</a>";
+            }
+
+            $ref_element.attr("data-title", content);
+            $ref_element.tooltip({
+                "html" : true
+            });
+            $ref_element.tooltip('show');
+        } else {
+            // $ref_element.tooltip('toggle');
+        }
+        return false;
+    });
+}
+
